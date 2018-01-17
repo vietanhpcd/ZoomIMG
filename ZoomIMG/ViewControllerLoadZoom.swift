c4dbd617861761ba15c28fbce6ca14cc480e9eca
vietanh <vietanh.pcd@gmail.com>
2018-01-18 00:23:39 +0700

    post

--- ZoomIMG/ViewControllerLoadZoom.swift
+++ ZoomIMG/ViewControllerLoadZoom.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewControllerLoadZoom: UIViewController, UIScrollViewDelegate {
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
             scrollView.minimumZoomScale = 1.0
@@ -22,22 +23,25 @@ class ViewControllerLoadZoom: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchImage()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let imageUrl = URL(string: "https://www.wallpaperup.com/uploads/wallpapers/2014/11/17/519385/82b58deee6b45bfbcf86c42837e8e812.jpg")!
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            let imageData = NSData(contentsOf: imageUrl)!
-            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
-            
-            DispatchQueue.main.async {
-                imageView.image = UIImage(data: imageData as Data)
-                imageView.contentMode = UIViewContentMode.scaleAspectFit
-                self.view.addSubview(imageView)
-                self.imgView = imageView
+    // Private: chỉ gọi dc ở trong class nội tại, các class khác không gọi  được
+    private func fetchImage() {
+        if let url = URL(string: "https://www.wallpaperup.com/uploads/wallpapers/2014/11/17/519385/82b58deee6b45bfbcf86c42837e8e812.jpg") {
+            // Hiển thị Spinder khi load ảnh
+            spinner.startAnimating()
+            DispatchQueue.global(qos: .userInitiated).async {
+                let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+                if let imageData = NSData(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        imageView.image = UIImage(data: imageData as Data)
+                        imageView.contentMode = UIViewContentMode.scaleAspectFit
+                        self.view.addSubview(imageView)
+                        self.imgView = imageView
+                        // Tắt Spinder khi load xong
+                        self.spinner.stopAnimating()
+                    }
+                }
             }
         }
     }
--- ZoomIMG/ViewControllerPinch.swift
+++ ZoomIMG/ViewControllerPinch.swift
@@ -18,12 +18,12 @@ class ViewControllerPinch: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Load image cách 1
-        get_image("https://www.wallpaperup.com/uploads/wallpapers/2014/11/17/519385/82b58deee6b45bfbcf86c42837e8e812.jpg", imageViewAbove)
-        get_image("https://www.wallpaperup.com/uploads/wallpapers/2013/06/17/103939/aacab500845f96df64496793afdb2f89.jpg", imageViewBelow)
+        getImage("https://www.wallpaperup.com/uploads/wallpapers/2014/11/17/519385/82b58deee6b45bfbcf86c42837e8e812.jpg", imageViewAbove)
+        getImage("https://www.wallpaperup.com/uploads/wallpapers/2013/06/17/103939/aacab500845f96df64496793afdb2f89.jpg", imageViewBelow)
     }
     // Hiển thị ảnh bằng việc tải ảnh từ wed 
-    func get_image(_ url_str:String, _ imageView:UIImageView) {
-        let url:URL = URL(string: url_str)!
+    func getImage(_ urlText:String, _ imageView:UIImageView) {
+        let url = URL(string: urlText)!
         let session = URLSession.shared
         let task = session.dataTask(with: url, completionHandler: {
             (data, response, error) in
